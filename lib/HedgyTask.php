70fcb3f6965474a0ae162e8238def225657ad645
Aaron Parecki <aaron@parecki.com>
2015-04-01 08:15:52 -0500

    send replies!

--- lib/HedgyTask.php
+++ lib/HedgyTask.php
@@ -72,11 +72,16 @@ class HedgyTask {
       }
     }
 
-    $newest = ORM::for_table('entries')->where('user_id', $user_id)->where('processed', 0)->order_by_desc('published')->find_one();
+    // Find the newest alcoholic drink and process it
+    $newest = ORM::for_table('entries')
+      ->where('user_id', $user_id)
+      ->where('is_alcohol', 1)
+      ->where('processed', 0)
+      ->order_by_desc('published')
+      ->find_one();
     if($newest) {
       echo $newest->id."\n";
       $entry_id = $newest->id;
-      // Find the newest entry and process it
       self::process_entry($entry_id);
     }
     $set = ORM::for_table('entries')->where('user_id', $user_id)->where('processed', 0)->find_result_set();
@@ -107,15 +112,111 @@ class HedgyTask {
     }
   }
 
-  private static function process_entry($entry_id) {
-    // Check if the food/drink threshold is met
+  public static function process_entry($entry_id) {
+    // Check if the food/drink threshold is met for a particular entry
 
-    // Post a reply via Micropub
+    $drink = db\get_by_id('entries', $entry_id);
+    $user = db\get_by_id('users', $drink->user_id);
 
+    // Load the last 30 posts
+    $entries = ORM::for_table('entries')
+      ->where('user_id', $user->id)
+      ->where_lte('published', $drink->published)
+      ->order_by_desc('published')
+      ->limit(30)
+      ->find_many();
+
+    // In reverse chronological order, count the number of alcoholic drinks until a food post is encountered
+    $last_food = false;
+    $num_drinks = 0;
+
+    foreach($entries as $entry) {
+      if($entry->is_alcohol)
+        $num_drinks++;
+
+      if($entry->is_meal) {
+        $last_food = $entry;
+        break;
+      }
+    }
+
+    // If more than 2 drinks since food, and the food was more than a while ago, reply
+    if($last_food && $num_drinks > 2) {
+      echo "There were $num_drinks drinks since the last food post\n";
+      echo "The last food post was at " . $last_food->published . "\n";
+      echo "The last drink was at " . $drink->published . "\n";
+      $diff_seconds = strtotime($drink->published) - strtotime($last_food->published);
+      $rt = new \RelativeTime\RelativeTime();
+      $relative_date = $rt->convert($drink->published, $last_food->published);
+      echo "Last food was " . $diff_seconds . " seconds since the latest drink\n";
+      echo $relative_date . "\n";
+
+      if(strtotime($last_food->published) < strtotime($drink->published) - 3600*1) {
+        echo "Sending a reply!\n";
+
+        $replies = [];
+        $replies[] = 'I\'m sure that\'s a tasty ' . strtolower(self::remove_article($drink->drink)) . ' but it\'s been a while since you last ate, you should get some food!';
+        $replies[] = 'It\'s been a while since you last ate, might want to get some food to go with that ' . strtolower(self::remove_article($drink->drink)) . '!';
+        $replies[] = 'You last ate ' . strtolower(self::remove_article($last_food->food)) . ' ' . $relative_date . ', might want to get some more food!';
+
+        if($user->name) {
+          $replies[] = 'Hey ' . $user->name . ', you\'ve had a few drinks since you last ate!';
+        }
+
+        // Force this reply on the 4th drink
+        if($num_drinks == 4)
+          $replies = ['Four drinks since your last meal! You should eat something!'];
+
+        $sentence = '@' . friendly_url($user->url) . ' ' . self::choose($replies);
+        echo "posting: '$sentence'\n";
+
+        $post = self::micropub_post([
+          'in-reply-to' => $drink->url,
+          'content' => $sentence
+        ]);
+        $location = $post['location'];
+        $reply = db\create('replies');
+        $reply->user_id = $user->id;
+        $reply->in_reply_to_id = $drink->id;
+        $reply->content = $sentence;
+        $reply->url = $location;
+        $reply->date_created = db\now();
+        $reply->save();
+      }
+
+    }
   }
 
-  private static function micropub_post() {
+  private static function choose($arr) {
+    return $arr[array_rand($arr)];
+  }
 
+  private static function remove_article($str) {
+    return preg_replace('/^an? /', '', $str);
+  }
+
+  private static function micropub_post($params) {
+    $ch = curl_init(Config::$micropub_server);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+      'Authorization: Bearer ' . Config::$micropub_access_token
+    ));
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array_merge(['h'=>'entry'], $params)));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HEADER, true);
+    $response = curl_exec($ch);
+    $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+    $headers = trim(substr($response, 0, $header_size));
+    $location = false;
+    if(preg_match('/Location: (.+)/', $headers, $match)) {
+      $location = $match[1];
+    }
+    return [
+      'status' => curl_getinfo($ch, CURLINFO_HTTP_CODE),
+      'headers' => $headers,
+      'body' => substr($response, $header_size),
+      'location' => $location
+    ];
   }
   
 }
